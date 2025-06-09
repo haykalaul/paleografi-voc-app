738a552af3d06f3c2a7c8958e158fa50c2267c81
Haykal-Proge <haikalaulilalbab@gmail.com>
2025-06-09 21:16:29 +0700

    Fix: Update ALLOWED_HOSTS and Vercel configuration for correct application entry point

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'https://paleografi-voc-7qzrnyfw8-aulilalbabs-projects.vercel.app'
+    'https://paleografi-voc-app.vercel.app', 
+]
